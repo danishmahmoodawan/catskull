dd1f3e4d93c3b50a06761139dafe4233ffa65759
danishmahmoodawan <dani.ciitwah@gmail.com>
2022-01-31 02:13:19 +0500

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,157 @@
+
+import { useState, useEffect } from 'react';
+function Home() {
+    
+  return (
+   <> 
+    
+	<section class="first_section"></section>
+	<section class="second_section">
+		<div class="container second_con">
+			<div class="row">
+				<div class="col-12 col-md-6 content_col"> 
+					<p>Free</p>
+					<p>0/888</p>
+					<p>0.033 ETH</p>
+					<p>0/8000</p>
+					<p>10 MAX</p>
+					<p>PER TX</p>
+
+				</div>
+				<div class="col-12 col-md-6 img_col">
+					<div class="row text-center">
+						<div class="col-3"> <a href="#"><img src="assets/images/plus.svg" alt=""/></a></div>
+						<div class="col-6"><img src="assets/images/group01.png" alt=""/></div>
+						<div class="col-3"><a href="#"><img src="assets/images/minus.svg" alt=""/></a></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="third_section"> 
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-md-4"> <img class="ppaartboard" src="assets/images/ppaartboard01.svg" alt=""/></div>
+				<div class="col-12 col-md-4 left_content">
+					<h1>FAQ</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec enim erat. Donec scelerisque dui metus, pretium elementum urna rhoncus commodo. 
+
+Morbi venenatis id tortor quis mattis. Vestibulum hendrerit eros id luctus lacinia. Morbi sed purus in metus congue iaculis. Curabitur accumsan neque eget justo molestie rutrum. Sed malesuada nec velit et sagittis. Integer sed quam sit amet nunc pulvinar tincidunt.
+					</p>
+				</div>
+				<div class="col-12 col-md-4 right_content">
+					<p>SOME QUESTION HERE AND SOME QUESTION HERE</p>
+					<p>SOME QUESTION HERE AND SOME QUESTION HERE</p>
+					<p>SOME QUESTION HERE AND SOME QUESTION HERE</p>
+					<p>SOME QUESTION HERE AND SOME QUESTION HERE</p>
+					<p>SOME QUESTION HERE AND SOME QUESTION HERE</p>
+					<p>SOME QUESTION HERE AND SOME QUESTION HERE</p>
+					<p>SOME QUESTION HERE AND SOME QUESTION HERE</p>
+					<p>SOME QUESTION HERE AND SOME QUESTION HERE</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="forth_section"></section>
+	<section class="fifth_section"></section>
+	<section class="sixth_section">
+	<div class="container content_container">
+		<div class="row">
+			<div class="col-12 col-md-8">
+				<h2>INSPIRATION</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec enim erat. Donec scelerisque dui metus, pretium elementum urna rhoncus commodo.</p>
+			</div>
+			<div class="col-12 col-md-4 skull_col"> 
+				<img class="skull" src="assets/images/skull.svg" alt=""/>
+			</div>
+		</div>
+	</div>
+	<div class="container rapid_container">
+		<h2 class="text-center">RARITY CHART</h2>
+		<div class="row">
+			<div class="col-12 col-md-4 rapid_4_col">
+				<img class="group02" src="assets/images/group02.png" alt=""/>
+			</div>
+			<div class="col-12 col-md-8 rapid_8_col">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec enim erat. Donec scelerisque dui metus, pretium elementum urna rhoncus commodo. 
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 col-md-8 rapid_8_col">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec enim erat. Donec scelerisque dui metus, pretium elementum urna rhoncus commodo. 
+				</p>
+			</div>
+			<div class="col-12 col-md-4 rapid_4_col">
+				<img class="group02" src="assets/images/group02.png" alt=""/>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 col-md-4 rapid_4_col">
+				<img class="group02" src="assets/images/group02.png" alt=""/>
+			</div>
+			<div class="col-12 col-md-8 rapid_8_col">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec enim erat. Donec scelerisque dui metus, pretium elementum urna rhoncus commodo. 
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 col-md-8 rapid_8_col">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec enim erat. Donec scelerisque dui metus, pretium elementum urna rhoncus commodo. 
+				</p>
+			</div>
+			<div class="col-12 col-md-4 rapid_4_col">
+				<img class="group02" src="assets/images/group02.png" alt=""/>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 col-md-4 rapid_4_col">
+				<img class="group02" src="assets/images/group02.png" alt=""/>
+			</div>
+			<div class="col-12 col-md-8 rapid_8_col">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec enim erat. Donec scelerisque dui metus, pretium elementum urna rhoncus commodo. 
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 col-md-8 rapid_8_col">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec enim erat. Donec scelerisque dui metus, pretium elementum urna rhoncus commodo. 
+				</p>
+			</div>
+			<div class="col-12 col-md-4 rapid_4_col">
+				<img class="group02" src="assets/images/group02.png" alt=""/>
+			</div>
+		</div>
+	</div>
+	</section>
+	<section class="seven_section">
+		<div class="container-fluid">
+			<div class="seven_col">
+				<img class="six_img" src="assets/images/top-left.svg" alt="top-left"/>
+				<img class="six_img" src="assets/images/center.svg" alt="center"/>
+				<img class="six_img" src="assets/images/center.svg" alt="center"/>
+				<img class="six_img" src="assets/images/top-right.svg" alt="top-right"/>
+			</div>
+			<div class="seven_col">
+				<img class="six_img" src="assets/images/bottom-left.svg" alt="top-left"/>
+				<img class="six_img" src="assets/images/center.svg" alt="center"/>
+				<img class="six_img" src="assets/images/center.svg" alt="center"/>
+				<img class="six_img" src="assets/images/bottom-right.svg" alt="top-right"/>
+			</div>
+		</div>
+	</section>
+	<section class="eight_section">
+		<div class="container"><h2>Title Here</h2></div>
+	</section>
+	</>
+  );
+}
+
+export default Home;
